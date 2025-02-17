a4cf294b3dc3808ab86e51e049e07451bdd5b70e
Trevor <trev@trevrosa.dev>
2025-02-17 09:40:07 +0800

    Todo some ios to try to make it build

--- composeApp/src/iosMain/kotlin/org/trevor/pcup/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/org/trevor/pcup/Platform.ios.kt
@@ -1,20 +1,7 @@
 package org.trevor.pcup
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import co.touchlab.kermit.Logger
-import com.patrykandpatrick.vico.multiplatform.cartesian.CartesianChartHost
-import com.patrykandpatrick.vico.multiplatform.cartesian.axis.HorizontalAxis
-import com.patrykandpatrick.vico.multiplatform.cartesian.axis.VerticalAxis
-import com.patrykandpatrick.vico.multiplatform.cartesian.data.CartesianChartModelProducer
-import com.patrykandpatrick.vico.multiplatform.cartesian.data.columnSeries
-import com.patrykandpatrick.vico.multiplatform.cartesian.layer.rememberColumnCartesianLayer
-import com.patrykandpatrick.vico.multiplatform.cartesian.rememberCartesianChart
 import platform.UIKit.UIDevice
 
 // @Composable unused here, but required because of the `expect`ed function
@@ -37,6 +24,7 @@ class IOSPlatform : Platform {
         }
 
     override fun getScreenTimes(): List<ScreenTime>? {
+//        platform.ScreenTime.STScreenTimeConfiguration
         TODO("Not yet implemented")
     }
 
@@ -47,28 +35,6 @@ class IOSPlatform : Platform {
 
 @Composable
 actual fun Graph(data: Collection<Number>) {
-    Row(Modifier.fillMaxWidth(0.9F), horizontalArrangement = Arrangement.Center) {
-        val modelProducer = remember { CartesianChartModelProducer() }
-
-        LaunchedEffect(Unit) {
-            modelProducer.runTransaction {
-                columnSeries { series(data) }
-                Logger.d("created column series")
-            }
-        }
-
-        CartesianChartHost(
-            rememberCartesianChart(
-                rememberColumnCartesianLayer(),
-                startAxis = VerticalAxis.rememberStart(guideline = null, tick = null, label = null),
-                bottomAxis = HorizontalAxis.rememberBottom(
-                    guideline = null,
-                    tick = null,
-                    label = null
-                ),
-            ), modelProducer
-        )
-        Logger.i("chart host init")
-    }
+    TODO("Not yet implemented")
 }
 
