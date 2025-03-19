79d839afd1e644c69dbb6227984b3a72bccad320
Trevor <trev@trevrosa.dev>
2025-03-19 13:07:39 +0800

    Password

--- composeApp/src/commonMain/kotlin/org/trevor/pcup/screens/Login.kt
+++ composeApp/src/commonMain/kotlin/org/trevor/pcup/screens/Login.kt
@@ -1,10 +1,14 @@
 package org.trevor.pcup.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,7 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.datastore.preferences.core.edit
@@ -43,15 +51,48 @@ fun Login(httpClient: HttpClient): Boolean {
 
         Spacer(Modifier.height(20.dp))
 
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        val usernameInteraction = remember { MutableInteractionSource() }
+        val usernameFocused by usernameInteraction.collectIsFocusedAsState()
+
         var username by remember { mutableStateOf("") }
         val setUsername = { s: String -> if (s.length <= 64) username = s }
-        TextField(username, setUsername, placeholder = { Text("username") })
+        TextField(
+            username,
+            setUsername,
+            placeholder = { Text("username") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Text
+            ),
+            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+            interactionSource = usernameInteraction,
+            singleLine = true,
+        )
 
         Spacer(Modifier.height(10.dp))
 
+        val passwordInteraction = remember { MutableInteractionSource() }
+        val passwordFocused by passwordInteraction.collectIsFocusedAsState()
+
         var password by remember { mutableStateOf("") }
         val setPassword = { s: String -> if (s.length <= 64) password = s }
-        TextField(password, setPassword, placeholder = { Text("password") })
+        TextField(
+            password,
+            setPassword,
+            placeholder = { Text("password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+            interactionSource = passwordInteraction,
+            singleLine = true,
+        );
 
         Spacer(Modifier.height(10.dp))
 
